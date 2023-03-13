9ef4a3f499eda6342e955e2e30ecabbc104b27df
Oleksii <lesha.avdeev@gmail.com>
2023-03-13 17:17:08 +0200

    Create initial script structure

---
+++ script/script.js
@@ -0,0 +1,51 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+const hobbit = new Book("Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+const catcherInTheRye = new Book(
+  "The Catcher in the Rye",
+  "J.D. Salinger",
+  277,
+  "not read yet"
+);
+const toKillAMockingbird = new Book(
+  "To Kill a Mockingbird",
+  "Harper Lee",
+  281,
+  "not read yet"
+);
+const nineteenEightyFour = new Book(
+  "1984",
+  "George Orwell",
+  328,
+  "not read yet"
+);
+const prideAndPrejudice = new Book(
+  "Pride and Prejudice",
+  "Jane Austen",
+  279,
+  "not read yet"
+);
+const theGreatGatsby = new Book(
+  "The Great Gatsby",
+  "F. Scott Fitzgerald",
+  180,
+  "not read yet"
+);
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+addBookToLibrary(hobbit);
+addBookToLibrary(catcherInTheRye);
+addBookToLibrary(toKillAMockingbird);
+addBookToLibrary(nineteenEightyFour);
+addBookToLibrary(prideAndPrejudice);
+addBookToLibrary(theGreatGatsby);
